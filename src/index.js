4e9ba8fa5dd161fb59468bb06ffcea8f3b478d8f
k4itrun <103044629+k4itrun@users.noreply.github.com>
2023-07-12 21:48:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,27 @@
 "use strict";
 
-const name = "[replit-uptimer]"; //zzz...
-
-/**
-function config(options = {}) {
- let port = options.port || 3000 || 8080;
- let createURL = options.path ? options.path.toString() : "/";
- let createResponse = options.message ? options.message.toString() : config.on;
- const request = async (req, res) => {
-  if (options.debug) console.log(`${colors.green.bold(`::debug:: ${name} => ${req.method.toLowerCase()} ${req.url}`)}`);
-  if (req.url === createURL) { res.writeHead(200); return res.end(createResponse); }
- };
- **/
-
+const name = "[replit.uptimer]";
 const colors = require("colors");
 const { createServer } = require("node:http");
 
 function config(options = {}) {
- const port = options.port || 3000 || 8080;
+ const port = options.port;
  let createURL = options.path ? options.path.toString() : "/";
- let createResponse = options.message ? options.message.toString() : "!ON FIRE¡";
+ let createResponse = options.message ? options.message.toString() : "This message can be configured.";
  const request = async (req, res) => {
   if (options.debug) console.log(`${colors.green.bold(`:: debug :: ${name} => ${req.method.toLowerCase()} ${req.url}`)}`);
   if (req.url === createURL) { res.writeHead(200); return res.end(createResponse); }
  };
 
  const server = createServer(request);
- server.listen(port, () => { if (options.debug) console.log(`${colors.green.bold(`:: debug :: ${name} => Servidor online en el pueto: ${port}`)}`);});
+ server.listen(port, () => { if (options.debug) console.log(`${colors.green.bold(`:: debug :: ${name} => Online server in the port: ${port}`)}`);});
 }
 
 console.log(`${colors.green.bold(`:: server :: ${name} => online`)}`);
 
-module.exports = { config };
+module.exports = {
+ config // => Soon more...
+};
 
 /**
  * @INFO
